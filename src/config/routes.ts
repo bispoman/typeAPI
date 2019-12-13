20117ea730cc065e30dc23446d7a4fbf2f622ff1
Bispo <daniel@bisponet.com>
2019-12-13 18:17:50 -0300

    Creating a router to store all needed routes.

---
+++ src/config/routes.ts
@@ -0,0 +1,31 @@
+import * as Router from "koa-router";
+
+export const router = new Router({
+    prefix: '/api'
+});
+
+router.get("/ping", async (ctx, next) => {
+    ctx.body = { msg: "Pong"}
+
+    await next()
+});
+
+router.get("/char/:num", async (ctx, next) => {
+    const findName = ctx.params.get('num');
+    //mandar pro service de busca na swapi
+})
+
+router.get("/planet/:num", async (ctx, next) => {
+    const findName = ctx.params.get('num');
+    //mandar pro service de busca na swapi
+})
+
+router.get("/starship/:num", async (ctx, next) => {
+    const findName = ctx.params.get('num');
+    //mandar pro service de busca na swapi
+});
+
+router.get("/vehicle/:num", async (ctx, next) => {
+    const findName = ctx.params.get('num');
+    //mandar pro service de busca na swapi
+})
