83e68fcc20cf556ceac56b65fa500addafd3e6c9
Bispo <daniel@bisponet.com>
2019-12-23 18:04:58 -0300

    Wiring um index to routes

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,29 @@
 import * as Koa from "koa";
-import * as Router from "koa-router";
+import { router } from "./config/routes"
 
 import * as logger from "koa-logger";
 import * as json from "koa-json";
 import * as bodyParser from "koa-bodyparser";
 
 const app = new Koa();
-const router = new Router();
+//const router = new Router();
 
-interface PostRequest {
-    name: string;
-};
+// interface PostRequest {
+//     name: string;
+// };
 
-//Ping endpoint
-router.get("/ping", async (ctx, next) => {
-    ctx.body = { msg: "Pong" };
+// //Ping endpoint
+// router.get("/ping", async (ctx, next) => {
+//     ctx.body = { msg: "Pong" };
 
-    await next();
-});
+//     await next();
+// });
 
-router.post("/posting", async (ctx, next) => {
-    const data = <PostRequest>ctx.request.body;
-    ctx.body = { name: data.name };
-    await next();
-})
+// router.post("/posting", async (ctx, next) => {
+//     const data = <PostRequest>ctx.request.body;
+//     ctx.body = { name: data.name };
+//     await next();
+// })
 
 //Middlewares
 app.use(json());
