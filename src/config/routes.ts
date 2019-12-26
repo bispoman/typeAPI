737c2a236f9e3889f69e87bdafe272199d375f29
Bispo <daniel@bisponet.com>
2019-12-26 17:24:45 -0300

    removing unnecessary awaits

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -7,19 +7,15 @@ export const router = new Router({
 
 router.get("/ping", async (ctx, next) => {
     ctx.body = { msg: "Pong"}
-
-    await next();
 });
 
 router.get("/char/:num", async (ctx, next) => {
     const findNum = ctx.params.get('num');
     if (!(accessService.validateReturns(findNum))) {
         ctx.status = 400;
-        await next();
     } else {
         ctx.body = accessService.getCharacters(findNum);
         ctx.status = 200;
-        await next();
     }
 })
 
@@ -27,11 +23,9 @@ router.get("/planet/:num", async (ctx, next) => {
     const findNum = ctx.params.get('num');
     if (!(accessService.validateReturns(findNum))) {
         ctx.status = 400;
-        await next();
     } else {
         ctx.body = accessService.getPlanet(findNum);
         ctx.status = 200;
-        await next();
     }
 })
 
@@ -39,10 +33,8 @@ router.get("/starship/:num", async (ctx, next) => {
     const findNum = ctx.params.get('num');
     if (!(accessService.validateReturns(findNum))) {
         ctx.status = 400;
-        await next();
     } else {
         ctx.body = accessService.getStarship(findNum);
         ctx.status = 200;
-        await next();
     }
 });
