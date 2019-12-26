8f2f4df2babab29b34af8d416b35ba40cee23abe
Bispo <daniel@bisponet.com>
2019-12-26 10:28:28 -0300

    cleaning old code on index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,6 @@ import * as json from "koa-json";
 import * as bodyParser from "koa-bodyparser";
 
 const app = new Koa();
-//const router = new Router();
-
-// interface PostRequest {
-//     name: string;
-// };
-
-// //Ping endpoint
-// router.get("/ping", async (ctx, next) => {
-//     ctx.body = { msg: "Pong" };
-
-//     await next();
-// });
-
-// router.post("/posting", async (ctx, next) => {
-//     const data = <PostRequest>ctx.request.body;
-//     ctx.body = { name: data.name };
-//     await next();
-// })
 
 //Middlewares
 app.use(json());
