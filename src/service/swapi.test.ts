b3e65e927b903697a0b2ca5c541dbf7ead21e917
Bispo <daniel@bisponet.com>
2020-01-02 13:24:06 -0300

    starting to add tests

---
+++ src/service/swapi.test.ts
@@ -0,0 +1,29 @@
+import * as swapi from "./swapi"
+
+import * as Nock from "nock";
+import Character from '../model/character';
+import * as jest from "jest";
+
+test('validate number', () => {
+    expect(swapi.validateReturns(7)).toBe(true);
+});
+
+test('validate null', () => {
+    expect(swapi.validateReturns(null)).toBe(false);
+});
+
+test('validate string', () => {
+    expect(swapi.validateReturns('what')).toBe(false);
+});
+
+const swapiReturn = "{\"name\":\"Luke Skywalker\",\"height\":\"172\",\"mass\":\"77\",\"hair_color\":\"blond\",\"skin_color\":\"fair\",\"eye_color\":\"blue\",\"birth_year\":\"19BBY\",\"gender\":\"male\",\"homeworld\":\"https://swapi.co/api/planets/1/\",\"films\":[\"https://swapi.co/api/films/2/\",\"https://swapi.co/api/films/6/\",\"https://swapi.co/api/films/3/\",\"https://swapi.co/api/films/1/\",\"https://swapi.co/api/films/7/\"],\"species\":[\"https://swapi.co/api/species/1/\"],\"vehicles\":[\"https://swapi.co/api/vehicles/14/\",\"https://swapi.co/api/vehicles/30/\"],\"starships\":[\"https://swapi.co/api/starships/12/\",\"https://swapi.co/api/starships/22/\"],\"created\":\"2014-12-09T13:50:51.644000Z\",\"edited\":\"2014-12-20T21:17:56.891000Z\",\"url\":\"https://swapi.co/api/people/1/\"}"
+const filteredReturn = { name: "Luke Skywalker", height: "172", mass: "77", hair_color: "blond", skin_color: "fair", eye_color: "blue", gender: "male", homeworld: "https://swapi.co/api/planets/1/"};
+
+test('charactersReturn', async() => {
+    Nock('https://swapi.com/api')
+        .get('/people/1')
+        .reply(200, swapiReturn);
+
+    const data = await swapi.getCharacters(1);
+    expect(data).toEqual(filteredReturn);
+});
